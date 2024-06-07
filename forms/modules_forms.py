6c94067420b91a7f64dc309b6636b98acdd15d2e
Dani4ka <daniilroitenberg@yandex.ru>
2024-06-07 22:49:39 +0300

    Добавление слов. Наконец-то!

--- forms/modules_forms.py
+++ forms/modules_forms.py
@@ -2,7 +2,44 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, EmailField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired
 
+
 class CreateModule(FlaskForm):
     name = StringField('Название модуля', validators=[DataRequired()])
     description = StringField('Описание модуля(пару слов)', validators=[DataRequired()])
     submit = SubmitField('Создать', validators=[DataRequired()])
+
+
+class AddWord(FlaskForm):
+    word1 = StringField('Слово' )
+    translation1 = StringField('Перевод')
+
+    word2 = StringField('Слово')
+    translation2 = StringField('Перевод')
+
+    word3 = StringField('Слово')
+    translation3 = StringField('Перевод')
+
+    word4 = StringField('Слово')
+    translation4 = StringField('Перевод')
+
+    word5 = StringField('Слово')
+    translation5 = StringField('Перевод')
+
+    word6 = StringField('Слово')
+    translation6 = StringField('Перевод')
+
+    word7 = StringField('Слово')
+    translation7 = StringField('Перевод')
+
+    word8 = StringField('Слово')
+    translation8 = StringField('Перевод')
+
+    word9 = StringField('Слово')
+    translation9 = StringField('Перевод')
+
+    word10 = StringField('Слово')
+    translation10 = StringField('Перевод')
+
+    save_btn = SubmitField('Сохранить')
+
+
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager, login_user, logout_user, current_user
 from data.db_session import create_session, global_init
 from data.modules import Module
 from data.users import User
-from forms.modules_forms import CreateModule
+from forms.modules_forms import CreateModule, AddWord
 from forms.user_form import LoginForm, RegisterForm, RegisterStudentForm
 
 app = Flask(__name__)
@@ -140,17 +140,23 @@ def add_module():
     form = CreateModule()
     if form.validate_on_submit():
         sess = create_session()
+
+        all_modules_names = sess.query(Module.name).all()
+
+        if form.name.data in all_modules_names:
+            return render_template('new_module.html', form=form,
+                                   message='Такой модуль уже есть. Придумайте другое название',
+                                   title='Создание модуля')
+
         module = Module(name=form.name.data,
                         description=form.description.data,
                         words=f'{form.name.data}.csv',
                         teacher=current_user.id)
+
         sess.add(module)
 
-
-        with open(f'static/words/{module.words}', 'w') as file:
-            writer = csv.DictWriter(file, fieldnames=['word', 'translation'], delimiter=';',
-                                    quoting=csv.QUOTE_NONNUMERIC)
-            writer.writeheader()
+        with open(f'static/words/{module.words}', 'w', newline='') as file:
+            pass
 
         for student in sess.query(User).filter(User.teacher == current_user.email):
             if student.modules:
@@ -160,7 +166,6 @@ def add_module():
             else:
                 student.modules = f'{module.id}: {0}'
 
-
         sess.commit()
 
         return redirect('/all_modules')
@@ -168,9 +173,53 @@ def add_module():
     return render_template('new_module.html', form=form)
 
 
+@app.route('/add_word/<int:module_id>', methods=['GET', 'POST'])
+def add_word(module_id):
+    form = AddWord()
+    if form.validate_on_submit():
+        sess = create_session()
+        module: Module = sess.query(Module).get(module_id)
+        with open(f'static/words/{module.words}', 'r', newline='') as file:
+            data = csv.DictReader(file, delimiter=';', quotechar='"')
+            previous_words = [line for line in data]
+
+        with open(f'static/words/{module.words}', 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'word', 'translation'], delimiter=';',
+                                    quoting=csv.QUOTE_NONNUMERIC)
+            writer.writeheader()
+
+            last_index = 1
+            if previous_words:
+                print(previous_words[-1])
+                last_index = int(previous_words[-1]['id']) + 1
+                print(last_index)
+
+            for line in previous_words:
+                writer.writerow(line)
+
+            for i in range(1, 11):
+                word = form.data[f'word{i}']
+                translation = form.data[f'translation{i}']
+                if word and translation:
+                    writer.writerow({'id': str(last_index), 'word': word, 'translation': translation})
+
+                    last_index += 1
+
+        return redirect('/all_modules')
+
+    return render_template('add_word.html', form=form, title='Добавление слов')
+
+
+@app.route('/edit_module/<int:module_id>', methods=['GET', 'POST'])
+def edit_module(module_id):
+    sess = create_session()
+    module = sess.query(Module).get(module_id)
+    return render_template('edit_module.html', module=module, title='Изменение модуля')
+
+
 def main():
     global_init('db/users.db')
-    app.run('127.0.0.1', 8000)
+    app.run('127.0.0.1', 5000)
 
 
 if __name__ == '__main__':
